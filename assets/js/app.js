770d94ce7383ee4df03d1da888d8e38ecece9b3f
Truowng <quockhanhtruong166@gmail.com>
2023-09-20 17:16:24 +0700

    add about us

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,9 +1,10 @@
-const swiper = new Swiper(".swiper", {
+const homePtSwiper = new Swiper("#home-pt .swiper", {
   // Optional parameters
   direction: "horizontal",
   loop: false,
   spaceBetween: 15,
   initialSlide: 2,
+  speed: 750,
   breakpoints: {
     // when window width is >= 576px
     576: {
@@ -11,6 +12,48 @@ const swiper = new Swiper(".swiper", {
       spaceBetween: 20,
       initialSlide: 1,
       loop: true,
+    },
+  },
+});
+
+const aboutAdvantageSwiper = new Swiper("#about-advantage .swiper", {
+  // Optional parameters
+  direction: "horizontal",
+  loop: false,
+  slidesPerView: 1,
+  speed: 750,
+  autoplay: {
+    delay: 3000,
+  },
+  breakpoints: {
+    991: {
+      slidesPerView: 3,
+      spaceBetween: 15,
+    },
+  },
+});
+
+const aboutCoursesSwiper = new Swiper("#about-courses .swiper", {
+  // Optional parameters
+  direction: "horizontal",
+  loop: false,
+  slidesPerView: 1,
+  speed: 750,
+  autoplay: {
+    delay: 3000,
+  },
+  navigation: {
+    nextEl: ".courses-button-next",
+    prevEl: ".courses-button-prev",
+  },
+  breakpoints: {
+    991: {
+      slidesPerView: 2,
+      spaceBetween: 15,
+    },
+    1199: {
+      slidesPerView: 3,
+      spaceBetween: 15,
     },
   },
 });
